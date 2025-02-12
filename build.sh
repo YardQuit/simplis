c48c85ba4272adf505deb9bcb3c36f1916220833
Michael Jones <yardquit@pm.me>
2025-02-12 23:48:34 +0100

    Adding 1password install

--- build.sh
+++ build.sh
@@ -20,12 +20,13 @@ dnf5 -y install ghostty
 dnf5 -y install starship
 
 ### INSTALLS RPMs
-# wget https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
-# mv 1password-latest.rpm /tmp/
+wget https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
+mv 1password-latest.rpm /tmp/
+ls -la /
 mkdir -p /var/opt
 ln -s /var/opt/ /opt
 ls -la /
-# dnf5 -y install /tmp/1password-latest.rpm
+dnf5 -y install /tmp/1password-latest.rpm
 
 ### FIXING ABRTD SERVICE FROM FAILING TO START
 # groupadd abrt
