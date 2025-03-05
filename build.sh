101b0c1de8f4dcb96be51e87bd5cfabfd512c5f2
Michael Jones <yardquit@pm.me>
2025-03-05 09:55:17 +0100

    Change ghostty copr from a-zhn to pgdev

--- build.sh
+++ build.sh
@@ -43,5 +43,5 @@ rm -rf /tmp/* || true
 rm -rf /var/!(cache)
 rm -rf /var/cache/!(rpm-ostree)
 rm -rf /etc/yum.repos.d/atim-starship-fedora-41.repo
-rm -rf /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:a-zhn:ghostty.repo
+rm -rf /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:pgdev:ghostty.repo
 rm -rf /etc/yum.repos.d/_copr_ryanabx-cosmic.repo
